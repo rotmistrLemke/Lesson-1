e70ad2b463e4befc93a7fe393d0003b1e6db18b2
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-03-27 20:45:34 +0300

    sem2task01

---
+++ Sem2Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+System.Random numSintezator = new System.Random();
+int rndNum = numSintezator.Next(10, 100);
+Console.WriteLine(rndNum);
+short firstNum = (short)(rndNum / 10);
+short secondNum = (short)(rndNum % 10);
+
+if (firstNum > secondNum)
+{
+    Console.WriteLine(firstNum);
+}
+else
+{
+    Console.WriteLine(firstNum);
+}
+
+//Вариант 2
+
+char[] digits = numSintezator.Next(10, 100).ToString().ToCharArray();
+Console.WriteLine(digits);
+firstNum = (int)(digits[0]) - 48;
+secondNum = (int)(digits[1]) - 48;
