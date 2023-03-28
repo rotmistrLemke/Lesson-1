6688b17282ac9f800f0d57cd079eec8ccdce665a
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-03-28 23:10:56 +0300

    task2

---
+++ Sem1Task02/Program.cs
@@ -0,0 +1,5 @@
+﻿int firstNum = int.Parse(Console.ReadLine() ?? "0");
+int secondNum = int.Parse(Console.ReadLine() ?? "0");
+
+string res = (firstNum>secondNum)?$"{firstNum} больше чем {secondNum}":$"{secondNum} больше чем {firstNum}";
+Console.WriteLine(res);
