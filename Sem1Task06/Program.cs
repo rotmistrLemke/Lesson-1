33a1ee95d8219827b1583b3f38aad847b178d1d3
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-03-29 00:00:47 +0300

    Task06

---
+++ Sem1Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿//Вводим число, для проверки на четность
+Console.WriteLine("Введите число, для проверки на четность");
+int inlineNumber = int.Parse(Console.ReadLine() ?? "0");
+
+//Проверяем на четность
+if (inlineNumber % 2 == 0)
+{
+    Console.WriteLine($"Число {inlineNumber} четное");
+}
+else
+{
+    Console.WriteLine($"Число {inlineNumber} нечетное");
+}
