48025857f701f72956d72e85fe6f0de53afd8a20
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-03-29 00:25:17 +0300

    Task08

---
+++ Sem1Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿//вводим число
+Console.WriteLine("Введите число");
+int inlineNimber = int.Parse(Console.ReadLine() ?? "0");
+//Объявляем счетчик
+int counter = 1;
+
+//Выводим числа
+Console.Write($"Четные числа от 1 до {inlineNimber} это: ");
+
+while (counter <= inlineNimber)
+{
+    if (counter % 2 == 0)
+    {
+        Console.Write(counter + " ");
+        counter++;
+    }
+    else
+    {
+        counter++;
+    }
+}
