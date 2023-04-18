2a1522008fb7606b02214fc74f1d8740706bc190
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-04-18 23:57:11 +0300

    task52

---
+++ Sem7Task52HW/Program.cs
@@ -0,0 +1,76 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+* Дополнительно диагональ выделить разным цветом. */
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод, генерирующий массив
+int[,] Gen2DArray(int countRow, int countColumn, int minValue, int maxValue)
+{
+    int[,] arr = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+void Print2DArray(int[,] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                Console.ForegroundColor = col[new Random().Next(0, 16)];
+                Console.Write(arr[i, j] + " ");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetColAVG(int[,] arr)
+{
+    double[] avgArr = new double[arr.GetLength(1)];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            avgArr[j] = avgArr[j] + arr[i, j];
+        }
+    }
+    for (int i = 0; i < avgArr.Length; i++)
+    {
+        Console.Write(avgArr[i]/avgArr.Length + "; ");
+    }
+
+}
+
+int row = ReadData("Введите количество строк: ");
+int col = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Gen2DArray(row, col, 1, 5);
+Print2DArray(arr2D);
+Console.Write("Среднее арифметическое столбцов: ");
+GetColAVG(arr2D);
