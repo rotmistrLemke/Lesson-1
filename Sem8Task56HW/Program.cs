bcf67e574896631f98ac23c6894c113774e71f11
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-04-23 20:27:28 +0300

    task56

---
+++ Sem8Task56HW/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод, генерирующий массив
+int[,] Gen2DArray(int countRow, int countColumn, int minValue, int maxValue)
+{
+    int[,] arr = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+void Print2DArray(int[,] arr)
+{
+    int minValue = int.MaxValue;
+    int sum = 0;
+    int resultString = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum += arr[i, j];
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+        if (sum < minValue)
+        {
+            minValue = sum;
+            resultString = i + 1;
+        }
+    }
+    Console.WriteLine("В строке "+resultString+ " сумма элеметов наименьшая");
+}
+
+int row = ReadData("Введите количество строк: ");
+int col = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Gen2DArray(row, col, 1, 10);
+Print2DArray(arr2D);
