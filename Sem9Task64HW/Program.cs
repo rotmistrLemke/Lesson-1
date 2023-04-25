bc1e7cdb0e30fa9849e763f4817a614a1edd95ac
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-04-25 22:51:06 +0300

    Task64,66

---
+++ Sem9Task64HW/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+
+void LineGenRec(int numN)
+{
+    Console.Write(numN + " ");
+    if(numN<=1)
+    {
+
+    }
+    else
+    {
+       LineGenRec(numN-1);
+    }
+}
+
+int num = ReadData("Введите число N: ");
+LineGenRec(num);
---
+++ Sem9Task66HW/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+
+int  SumRec(int m, int n)
+{
+    if (m < n)
+    {
+       return m + SumRec(m + 1, n);
+    }
+    else
+    {
+        return m;
+    }
+}
+
+int m = ReadData("Введите m: ");
+int n = ReadData("Введите n: ");
+
+
+Console.WriteLine(SumRec((m > n) ? n : m, (m > n) ? m : n));
